8afcc59cf7dcac39703f86e5507b56811a852fcf
Mustapha Mubarak <58697223+pythagoras-dev@users.noreply.github.com>
2021-12-29 14:40:48 +0100

    Revert "test"
    This reverts commit 6211d256c5edc903c3c28e86b3979f1ba1ea6b3b.

--- src/pages/collections.js
+++ src/pages/collections.js
@@ -6,19 +6,16 @@ import axios from "axios";
 const Collections = () => {
   const [allCollections, setAllCollections] = useState([]);
   const [filteredCollections, setFilteredCollections] = useState([]);
-  const [filters, setFilters] = useState({ s: "", sort: "" });
+  const [filters, setFilters] = useState({ s: "" });
 
   useEffect(() => {
-    getCollection();
-  }, []);
-
-  const getCollection = () => {
     axios
       .get("./items.json")
       .then((res) => setAllCollections(res.data))
       .then((res) => setFilteredCollections(res.data))
       .catch((err) => console.log(err));
-  };
+  }, []);
+
   useEffect(() => {
     let collections = allCollections.filter(
       (p) =>
