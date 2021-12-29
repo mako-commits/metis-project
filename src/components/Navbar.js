5f9b6c0b604cf1d4f6591a496d91478681a84ea6
Nabil Alamin <aminonimisi2@gmail.com>
2021-12-29 15:42:38 +0100

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import { ethers } from "ethers";
+import React from "react";
+import { Link } from "react-router-dom";
 
-import { METIS_TESTNET_STARDUST } from '../constants/network'
 
 const Navbar = ({ toggle }) => {
 
