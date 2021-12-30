804ac1e2f37d9cd6a905d5abf79a8feec99579af
Mustapha Mubarak <58697223+pythagoras-dev@users.noreply.github.com>
2021-12-30 10:55:15 +0100

    added pop up feature to art card

--- src/components/ArtCard/ArtCard.jsx
+++ src/components/ArtCard/ArtCard.jsx
@@ -1,16 +1,60 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Modal from "react-modal";
+Modal.setAppElement("#root");
 const ArtCard = ({ image, name }) => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
   return (
-    <div className="mx-8 my-4 rounded-md">
-      <img
-        loading=""
-        className="lg:h-72 md:h-60 sm:h-48 w-auto bg-gray-200 object-contain object-center rounded-md shadow-md"
-        src={image}
-        alt="blog"
-      />
-      <p className="mt-4">{name}</p>
-    </div>
+    <>
+      <div
+        className="mx-8 my-4 rounded-md"
+        onClick={() => setModalIsOpen(true)}
+      >
+        <img
+          loading=""
+          className="lg:h-72 md:h-60 sm:h-48 w-auto bg-gray-200 object-contain object-center rounded-md shadow-md"
+          src={image}
+          alt="blog"
+        />
+        <p className="mt-4">{name}</p>
+      </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={() => setModalIsOpen(false)}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0.8)",
+          },
+          content: {},
+        }}
+      >
+        <button
+          onClick={() => setModalIsOpen(false)}
+          className="bg-red-400 px-5 py-3 rounded-md"
+        >
+          Close
+        </button>
+        <div className="p-4 md:w-1/3">
+          <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+            <img
+              className="lg:h-48 md:h-36 w-full object-cover object-center"
+              src={image}
+              alt="blog"
+            />
+            <div className="p-6">
+              <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                Rare Piece
+              </h2>
+              <h1 className="title-font text-lg font-medium text-gray-900 mb-3">
+                {name}
+              </h1>
+              <p className="leading-relaxed mb-3">background: 30%</p>
+              <p className="leading-relaxed mb-3">rare eye: 33%</p>
+              <p className="leading-relaxed mb-3">cloth: 21%</p>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </>
   );
 };
 
--- src/pages/collections.js
+++ src/pages/collections.js
@@ -1,37 +1,33 @@
 import React, { useState } from "react";
-import { metadata } from '../constants/metadata'
+import { metadata } from "../constants/metadata";
 import ArtCard from "../components/ArtCard/ArtCard";
 
 const Collections = () => {
-  
-  const metaLength = metadata.length
-  const [currentIndex, setCurrentindex] = useState(11)
+  const metaLength = metadata.length;
+  const [currentIndex, setCurrentindex] = useState(11);
 
   const add = () => {
-    if(currentIndex < metaLength){
-      setCurrentindex(currentIndex+11)
-    }else if(currentIndex === metaLength){
-      setCurrentindex(11)
+    if (currentIndex < metaLength) {
+      setCurrentindex(currentIndex + 11);
+    } else if (currentIndex === metaLength) {
+      setCurrentindex(11);
     }
-  } 
+  };
 
   return (
     <div className="grid place-items-center my-10">
-      <div className = "container m-auto flex flex-wrap justify-center">
-        {
-          metadata.slice(0,currentIndex).map((data) => (
-            <ArtCard
-              key = {data.dna}
-              image= {data.image}
-              name = {data.name}
-            />
-          ))
-        }   
+      <div className="container m-auto flex flex-wrap justify-center">
+        {metadata.slice(0, currentIndex).map((data) => (
+          <ArtCard key={data.dna} image={data.image} name={data.name} />
+        ))}
       </div>
-      <button 
-        className="lg:mt-2 xl:mt-6 flex-shrink-0 inline-flex text-white bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 border-0 py-2 px-6  rounded "
+      <button
+        className="lg:mt-2 xl:mt-6 flex-shrink-0 inline-flex text-white bg-gradient-to-r from-purple-800 via-purple-800 to-purple-800 border-0 py-2 px-6  rounded "
         onClick={add}
-      > Load More </button>       
+      >
+        {" "}
+        Load More{" "}
+      </button>
     </div>
   );
 };
