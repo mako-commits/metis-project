08bd8e5405a0996e80dad01c5bb4e059abc8e853
Mustapha Mubarak <58697223+pythagoras-dev@users.noreply.github.com>
2021-12-30 11:40:51 +0100

    refactored popup

--- src/components/ArtCard/ArtCard.jsx
+++ src/components/ArtCard/ArtCard.jsx
@@ -1,3 +1,4 @@
+import autoprefixer from "autoprefixer";
 import React, { useState } from "react";
 import Modal from "react-modal";
 Modal.setAppElement("#root");
@@ -18,25 +19,46 @@ const ArtCard = ({ image, name }) => {
         <p className="mt-4">{name}</p>
       </div>
       <Modal
+        className="popup"
         isOpen={modalIsOpen}
         onRequestClose={() => setModalIsOpen(false)}
         style={{
           overlay: {
             backgroundColor: "rgba(0, 0, 0, 0.8)",
           },
-          content: {},
+          content: {
+            display: "flex",
+            flexDirection: "column",
+            width: "80vw",
+            height: "auto",
+            margin: "auto",
+            marginTop: "50px",
+            backgroundColor: "#fff",
+          },
         }}
       >
-        <button
-          onClick={() => setModalIsOpen(false)}
-          className="bg-red-400 px-5 py-3 rounded-md"
-        >
-          Close
-        </button>
-        <div className="p-4 md:w-1/3">
-          <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
+        <div className="popup-close-btn">
+          <button
+            onClick={() => setModalIsOpen(false)}
+            className="px-5 py-3 rounded-md"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              class="h-6 w-6 text-red-700"
+              viewBox="0 0 1792 1792"
+            >
+              <path d="M1490 1322q0 40-28 68l-136 136q-28 28-68 28t-68-28l-294-294-294 294q-28 28-68 28t-68-28l-136-136q-28-28-28-68t28-68l294-294-294-294q-28-28-28-68t28-68l136-136q28-28 68-28t68 28l294 294 294-294q28-28 68-28t68 28l136 136q28 28 28 68t-28 68l-294 294 294 294q28 28 28 68z"></path>
+            </svg>
+          </button>
+        </div>
+
+        <div className="p-4 ">
+          <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden flex flex-col lg:flex-row">
             <img
-              className="lg:h-48 md:h-36 w-full object-cover object-center"
+              className="lg:h-96 md:h-36 lg:w-1/2 object-cover object-center"
               src={image}
               alt="blog"
             />
