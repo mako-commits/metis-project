b93dff4e3603aa13d5b699cd84f0cca7c53a32cf
arndom <aminonimisi2@gmail.com>
2022-01-03 00:01:14 +0100

    display offers

--- src/pages/views/offer.js
+++ src/pages/views/offer.js
@@ -1,57 +1,53 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import DashNav from "../../components/DashNav";
+import { db } from "../../config/firebase";
 
 const Offer = () => {
   const [balance, setBalance] = useState(0);
+  const [data, setData] = useState([])
+  const [data1, setData1] = useState([])
 
-  const people = [
-    {
-      name: "The Hive Restaurant",
-      message: "Yo!.........I would like this piece",
-      offer: "10% off drinks for 300 CNTM every Wednesday",
-      price: 300,
-      email: "hi@hiverestaurant.com",
-      image:
-        "https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
-    },
-    {
-      name: "The Palms Hotel",
-      message: "I want this item",
-      offer: " 15% off on weekends for 500 CNTM",
-      price: 500,
-      email: "contact@palmshotel.com",
-      image:
-        "https://images.unsplash.com/photo-1522798514-97ceb8c4f1c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
-    },
-    {
-      name: "Baccd Resort",
-      message: "I want this item",
-      offer: "75 CNTM for each referral",
-      price: 75,
-      email: "info@baccdresort.com",
-      image:
-        "https://images.unsplash.com/photo-1563911302283-d2bc129e7570?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8aG90ZWx8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-    },
-    {
-      name: "Ekpoma Mall",
-      message: "I want this item",
-      offer: "Valley parking for 600 CNTM",
-      price: 600,
-      email: "hello@ekoomamall.com",
-      image:
-        "https://media.istockphoto.com/photos/busy-shopping-mall-picture-id181996772?b=1&k=20&m=181996772&s=170667a&w=0&h=pbqpxTmkQgXm2STifeoshuGzQFrzyECtOFkKeSUC8TA=",
-    },
-  ];
+  const [displayData, setDisplayData] = useState([])
+
+  useEffect(() => {
+    //hotel
+    db.collection("Businesses")
+      .doc("0x8c12d017b5441c735050d13d034e19d459ebd33a")
+      .collection("deals")
+      .onSnapshot((snapshot) => {
+        let dna = snapshot.docs.map((offer) => offer.data());
+        setData(dna)
+      });
+
+      //rest.
+      db.collection("Businesses")
+      .doc("0xfccd950fa8cbd332634bcc57809a8a9a0496d4b6")
+      .collection("deals")
+      .onSnapshot((snapshot) => {
+        let dna = snapshot.docs.map((offer) => offer.data());
+        setData1(dna)
+      });
+  },[])
+
+  useEffect(() => {
+    const _data = data.concat(data1)
+    setDisplayData(_data)
+  },[data, data1])
+
+
   const onAddFunds = () => {
     setBalance(balance + 50);
   };
+
   const onRedeem = (event) => {
     event.preventDefault();
     alert("Congrats you just redeemed offf");
   };
+
   return (
     <>
       <DashNav />
+      {console.log(displayData)}
       <main className="bg-gray-400">
         <div className=" max-w-7xl mx-auto py-6 sm:px-6 lg:px-8 flex flex-col ">
           <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
@@ -97,20 +93,20 @@ const Offer = () => {
                     </tr>
                   </thead>
                   <tbody className="bg-white divide-y divide-gray-200">
-                    {people.map((person) => (
+                    {displayData?.map((person) => (
                       <tr key={person.email}>
                         <td className="px-6 py-4 whitespace-nowrap">
                           <div className="flex items-center">
                             <div className="flex-shrink-0 h-10 w-10">
                               <img
                                 className="h-10 w-10 rounded-full"
-                                src={person.image}
+                                src={`${data.filter((item) => item.info === person.info)}` ? "https://images.unsplash.com/photo-1522798514-97ceb8c4f1c8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGhvdGVsfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" : "https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8cmVzdGF1cmFudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"}
                                 alt=""
                               />
                             </div>
                             <div className="ml-4">
                               <div className="text-sm font-medium text-gray-900">
-                                {person.name}
+                                {`${data.filter((item) => item.info === person.info)}` ? "Hotel" : "Restaurant"}
                               </div>
                               <div className="text-sm text-gray-500">
                                 {person.email}
@@ -120,17 +116,17 @@ const Offer = () => {
                         </td>
                         <td className="px-6 py-4 whitespace-nowrap">
                           <div className="text-sm text-gray-900">
-                            {person.price}
+                            {person.value}
                           </div>
                         </td>
                         <td className="px-6 py-4 whitespace-nowrap">
                           <div className="text-sm text-gray-500">
-                            {person.offer}
+                            {person.info}
                           </div>
                         </td>
 
                         <td className="px-2 py-4 whitespace-nowrap text-sm text-gray-500">
-                          {balance > person.price ? (
+                          {balance > person.value ? (
                             <button
                               onClick={onRedeem}
                               className="lg:mt-2 xl:mt-0 mr-2 flex-shrink-0 inline-flex text-white bg-green-800 border-0 py-2 px-6 focus:outline-none hover:bg-green-400 rounded"
@@ -151,13 +147,6 @@ const Offer = () => {
             </div>
           </div>
         </div>
-        {/* <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          <div className="px-4 py-6 sm:px-0">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96 flex items-center justify-center">
-              <h1 className="italic text-xl">Wow....Such empty</h1>
-            </div>
-          </div>
-        </div> */}
       </main>
     </>
   );
